2f021f45d0217d0ff7b1e040a8e7e18ac0183083
Haley DeKeyser <hcd09@live.com>
2019-05-24 03:47:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,9 @@
 class CashRegister
-  attr_accessor :total
+  attr_accessor :total, :employee_discount
   
-  def initialize(employee_discount)
+  def initialize(employee_discount = 0)
     @total = 0
+    @employee_discount = employee_discount
   end
   
 end
